294b512f4c392f5cc2a85c3a63e18bf1b490341b
onirutlA <alturino001@gmail.com>
2022-05-23 14:32:03 +0700

    add: bind bottomNav with navController

--- app/src/main/java/com/ioh_c22_h2_4/hy_ponics/MainActivity.kt
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/MainActivity.kt
@@ -2,15 +2,22 @@ package com.ioh_c22_h2_4.hy_ponics
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.ioh_c22_h2_4.hy_ponics.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private val binding by lazy { ActivityMainBinding.inflate(layoutInflater) }
+    private val navHostFragment by lazy { supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment }
+    private val navController by lazy { navHostFragment.findNavController() }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        binding.bottomNav.setupWithNavController(navController)
+
     }
 }
