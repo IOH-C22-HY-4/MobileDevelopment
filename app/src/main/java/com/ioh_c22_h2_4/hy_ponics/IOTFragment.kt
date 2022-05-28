8f3a1ed7e20d2884884e32c5e0e4e163337dae59
Ade Adam Nisa Sabrina <adeadamnisa@gmail.com>
2022-05-28 20:02:19 +0700

    Revisi About Fragment (4)

--- app/src/main/java/com/ioh_c22_h2_4/hy_ponics/IOTFragment.kt
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/IOTFragment.kt
@@ -1,12 +1,13 @@
 package com.ioh_c22_h2_4.hy_ponics
 
 import android.os.Bundle
-import android.view.*
-import android.widget.Button
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.ioh_c22_h2_4.hy_ponics.databinding.FragmentIotBinding
 
-class IOTFragment : Fragment(),View.OnClickListener {
+class IOTFragment : Fragment() {
 
     private var _binding: FragmentIotBinding? = null
     private val binding get() = _binding!!
@@ -22,25 +23,9 @@ class IOTFragment : Fragment(),View.OnClickListener {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        val btnEditProfile: Button = view.findViewById(R.id.btnEditProfile)
-        btnEditProfile.setOnClickListener(this)
+
     }
 
-    override fun onClick(v: View) {
-        if (v.id == R.id.btnEditProfile) {
-            val mDetailProfileFragment = DetailProfileFragment()
-            val mFragmentManager = parentFragmentManager
-            mFragmentManager.beginTransaction().apply {
-                replace(
-                    R.id.IOTFragment,
-                    mDetailProfileFragment,
-                    DetailProfileFragment::class.java.simpleName
-                )
-                addToBackStack(null)
-                commit()
-            }
-        }
-    }
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
