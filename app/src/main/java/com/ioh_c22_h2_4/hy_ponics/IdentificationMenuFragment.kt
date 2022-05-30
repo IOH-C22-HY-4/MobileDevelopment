16643252cf214f35305a09485242992aa76e59f2
onirutlA <alturino001@gmail.com>
2022-05-30 15:17:17 +0700

    add: fab state

--- app/src/main/java/com/ioh_c22_h2_4/hy_ponics/IdentificationMenuFragment.kt
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/IdentificationMenuFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.animation.AnimationUtils
 import androidx.fragment.app.Fragment
 import com.ioh_c22_h2_4.hy_ponics.databinding.FragmentIdentificationMenuBinding
 
@@ -11,6 +12,22 @@ class IdentificationMenuFragment : Fragment() {
 
     private var _binding: FragmentIdentificationMenuBinding? = null
     private val binding get() = _binding!!
+
+    private var clicked: Boolean = false
+
+    private val rotateOpen by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.rotate_open
+        )
+    }
+
+    private val rotateClose by lazy {
+        AnimationUtils.loadAnimation(
+            requireContext(),
+            R.anim.rotate_close
+        )
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -24,6 +41,53 @@ class IdentificationMenuFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
+        binding.apply {
+            btnAdd.setOnClickListener {
+                onAddButtonClicked()
+            }
+            fabCamera.setOnClickListener {
+
+            }
+            fabGallery.setOnClickListener {
+
+            }
+        }
+    }
+
+    private fun onAddButtonClicked() {
+        setVisibility()
+        setAnimation()
+        clicked = !clicked
+    }
+
+    private fun setVisibility() {
+        if (!clicked) {
+            binding.run {
+                fabGallery.run {
+                    show()
+                }
+                fabCamera.run {
+                    show()
+                }
+            }
+        } else {
+            binding.run {
+                fabGallery.run {
+                    hide()
+                }
+                fabCamera.run {
+                    hide()
+                }
+            }
+        }
+    }
+
+    private fun setAnimation() {
+        if (!clicked) {
+            binding.btnAdd.startAnimation(rotateOpen)
+        } else {
+            binding.btnAdd.startAnimation(rotateClose)
+        }
     }
 
     override fun onDestroyView() {
