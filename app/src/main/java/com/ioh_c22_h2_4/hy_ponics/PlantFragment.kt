93bb302231a8948135f834c9ec8ec2520aeb19c3
onirutlA <alturino001@gmail.com>
2022-06-07 00:53:57 +0700

    refactor: code

--- app/src/main/java/com/ioh_c22_h2_4/hy_ponics/PlantFragment.kt
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/PlantFragment.kt
@@ -1,11 +1,13 @@
 package com.ioh_c22_h2_4.hy_ponics
 
 import android.graphics.Bitmap
+import android.graphics.BitmapFactory
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.core.graphics.drawable.toBitmap
+import androidx.core.graphics.scale
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
@@ -14,6 +16,8 @@ import com.ioh_c22_h2_4.hy_ponics.ml.Lettuceclass03V4Best
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
+import java.nio.ByteBuffer
+import java.nio.ByteOrder
 
 class PlantFragment : Fragment() {
 
@@ -22,9 +26,6 @@ class PlantFragment : Fragment() {
 
     private val sharedViewModel: SharedViewModel by activityViewModels()
 
-    private val model by lazy {
-        Lettuceclass03V4Best.newInstance(requireContext())
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -42,26 +43,63 @@ class PlantFragment : Fragment() {
             Glide.with(binding.ivPlantImage)
                 .load(it)
                 .into(binding.ivPlantImage)
+
+            val bitmap =
+                BitmapFactory.decodeStream(requireActivity().contentResolver.openInputStream(it))
+            analyze(bitmap)
         }
 
-        if (binding.ivPlantImage.drawable != null) {
-            var bitmap = binding.ivPlantImage.drawable.toBitmap()
-            bitmap = Bitmap.createScaledBitmap(bitmap, 150, 150, true)
+    }
 
-            val inputFeature0 = TensorBuffer.createFixedSize(intArrayOf(1, 150, 150, 3), DataType.FLOAT32)
-            val tensorImage = TensorImage(DataType.FLOAT32)
-            tensorImage.load(bitmap)
-            val byteBuffer = tensorImage.buffer
+    private fun analyze(bitmap: Bitmap) {
+        val model = Lettuceclass03V4Best.newInstance(requireContext())
 
-            inputFeature0.loadBuffer(byteBuffer)
+        val scaledBitmap = bitmap.scale(
+            150,
+            150,
+            true
+        ).copy(
+            Bitmap.Config.ARGB_8888,
+            true
+        )
 
-            val outputs = model.process(inputFeature0)
-            val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-
-            model.close()
-
-            binding.textView.text = "${outputFeature0.floatArray[0]} \n ${outputFeature0.floatArray[1]} \n ${outputFeature0.floatArray[2]} \n ${outputFeature0.floatArray[3]} \n ${outputFeature0.floatArray[4]} \n ${outputFeature0.floatArray[5]}"
+        val tensorImage = TensorImage(DataType.FLOAT32).apply { load(scaledBitmap) }
+        val byteBuffer = ByteBuffer.allocateDirect(150 * 150 * 3 * 4).apply {
+            put(tensorImage.buffer)
+            order(ByteOrder.nativeOrder())
         }
+
+        val input = TensorBuffer.createFixedSize(intArrayOf(1, 150, 150, 3), DataType.FLOAT32)
+            .apply {
+                loadBuffer(byteBuffer)
+            }
+
+        val outputs = model.process(input)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        var maxValue = 0.0
+        var index = 0
+        for (i in outputFeature0.floatArray.indices) {
+            Log.d("fragment", "${outputFeature0.floatArray[i].toDouble()}")
+            if (maxValue < outputFeature0.floatArray[i]) {
+                maxValue = outputFeature0.floatArray[i].toDouble()
+                index = i
+            }
+        }
+
+        val lettuceResult = when (index) {
+            0 -> "Selada Butterhead"
+            1 -> "Selada Green Romaine"
+            2 -> "Selada Hijau new grand rapids"
+            3 -> "Selada Keriting oakleaf green"
+            4 -> "Selada Merah red Rapids"
+            5 -> "Selada Red Romaine"
+            else -> "Unknown"
+        }
+
+        binding.tvPlantName.text = lettuceResult
+
+        model.close()
     }
 
     override fun onDestroyView() {
