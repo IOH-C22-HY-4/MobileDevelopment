a3cd4cf2476adb5cbb8f5ea0c215e6084a8e99b5
onirutlA <alturino001@gmail.com>
2022-06-08 11:15:55 +0700

    refactor: move model

--- app/src/main/java/com/ioh_c22_h2_4/hy_ponics/util/Constants.kt
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/util/Constants.kt
@@ -4,7 +4,7 @@ object Constants {
     const val FILENAME_FORMAT = "yyyy-MM-dd-HH-mm-ss-SSS"
     const val PHOTO_EXTENSION = ".jpg"
     const val OBJECT_COUNT = 6
-    const val MODEL_PATH = "lettuceclass_03_v4_best.tflite"
+    const val MODEL_PATH = "lettuceclass_03_v4.tflite"
     const val LABELS_PATH = "lettuceclass_03_v4_best.txt"
     const val ACCURACY_THRESHOLD = 0.5f
 }
