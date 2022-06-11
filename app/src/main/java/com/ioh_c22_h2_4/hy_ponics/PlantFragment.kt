742341df45de25e94e1539dc1b3150a7f77c83a6
onirutlA <alturino001@gmail.com>
2022-06-11 16:09:24 +0700

    refactor: change method from convert uri to bitmap to convert drawable to bitmap

--- app/src/main/java/com/ioh_c22_h2_4/hy_ponics/PlantFragment.kt
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/PlantFragment.kt
@@ -1,17 +1,18 @@
 package com.ioh_c22_h2_4.hy_ponics
 
 import android.graphics.Bitmap
-import android.graphics.BitmapFactory
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.graphics.drawable.toBitmap
 import androidx.core.graphics.scale
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import com.bumptech.glide.Glide
 import com.ioh_c22_h2_4.hy_ponics.databinding.FragmentPlantBinding
+import com.ioh_c22_h2_4.hy_ponics.ml.Model
 import org.tensorflow.lite.DataType
 import org.tensorflow.lite.support.image.TensorImage
 import org.tensorflow.lite.support.tensorbuffer.TensorBuffer
@@ -42,16 +43,17 @@ class PlantFragment : Fragment() {
             Glide.with(binding.ivPlantImage)
                 .load(it)
                 .into(binding.ivPlantImage)
+        }
 
-            val bitmap =
-                BitmapFactory.decodeStream(requireActivity().contentResolver.openInputStream(it))
+        binding.ivPlantImage.drawable?.let {
+            val bitmap = it.toBitmap()
             analyze(bitmap)
         }
 
     }
 
     private fun analyze(bitmap: Bitmap) {
-//        val model = Model.newInstance(requireContext())
+        val model = Model.newInstance(requireContext())
 
         val scaledBitmap = bitmap.scale(
             150,
@@ -61,8 +63,6 @@ class PlantFragment : Fragment() {
             Bitmap.Config.ARGB_8888,
             true
         )
-
-        binding.scaled.setImageBitmap(scaledBitmap)
 
         val tensorImage = TensorImage(DataType.FLOAT32).apply { load(scaledBitmap) }
         val byteBuffer = ByteBuffer.allocateDirect(150 * 150 * 3 * 4).apply {
@@ -76,36 +76,36 @@ class PlantFragment : Fragment() {
                 loadBuffer(byteBuffer)
             }
 
-//        val outputs = model.process(input)
-//        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
-//
-//        var maxValue = 0.0
-//        var index = 0
-//        for (i in outputFeature0.floatArray.indices) {
-//            Log.d("analyze", "${outputFeature0.floatArray[i].toDouble()}")
-//            if (maxValue < outputFeature0.floatArray[i]) {
-//                maxValue = outputFeature0.floatArray[i].toDouble()
-//                index = i
-//            }
-//        }
-//        Log.d("analyze", "maxvalue: $maxValue, index: $index")
-//
-//
-//        val labels = listOf(
-//            "Selada Butterhead",
-//            "Selada Green Romaine",
-//            "Selada Hijau new grand rapids",
-//            "Selada Keriting oakleaf green",
-//            "Selada Merah red Rapids",
-//            "Selada Red Romaine"
-//        )
-//
-//        val lettuceResult = labels[index]
-//
-//        Log.d("fragment", lettuceResult)
-//        binding.tvPlantName.text = lettuceResult
-//
-//        model.close()
+        val outputs = model.process(input)
+        val outputFeature0 = outputs.outputFeature0AsTensorBuffer
+
+        var maxValue = 0.0
+        var index = 0
+        for (i in outputFeature0.floatArray.indices) {
+            Log.d("analyze", "${outputFeature0.floatArray[i].toDouble()}")
+            if (maxValue < outputFeature0.floatArray[i]) {
+                maxValue = outputFeature0.floatArray[i].toDouble()
+                index = i
+            }
+        }
+        Log.d("analyze", "maxvalue: $maxValue, index: $index")
+
+
+        val labels = listOf(
+            "Selada Butterhead",
+            "Selada Green Romaine",
+            "Selada Hijau new grand rapids",
+            "Selada Keriting oakleaf green",
+            "Selada Merah red Rapids",
+            "Selada Red Romaine"
+        )
+
+        val lettuceResult = labels[index]
+
+        Log.d("fragment", lettuceResult)
+        binding.tvPlantName.text = lettuceResult
+
+        model.close()
     }
 
     override fun onDestroyView() {
