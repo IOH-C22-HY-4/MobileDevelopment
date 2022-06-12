80e096f3270f4f187d2fd1be681332c9adfba09d
Ade Adam Nisa Sabrina <adeadamnisa@gmail.com>
2022-06-12 19:55:39 +0700

    Integreasi Artikel di Home Fragment dari Firestore

---
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/Article.kt
@@ -0,0 +1,37 @@
+package com.ioh_c22_h2_4.hy_ponics
+
+import android.os.Parcel
+import android.os.Parcelable
+
+data class Article(
+    var title: String? = "",
+    var content: String? = "",
+    var img: String? = ""
+):Parcelable {
+    constructor(parcel: Parcel) : this(
+        parcel.readString(),
+        parcel.readString(),
+        parcel.readString()
+    ) {
+    }
+
+    override fun writeToParcel(parcel: Parcel, flags: Int) {
+        parcel.writeString(title)
+        parcel.writeString(content)
+        parcel.writeString(img)
+    }
+
+    override fun describeContents(): Int {
+        return 0
+    }
+
+    companion object CREATOR : Parcelable.Creator<Article> {
+        override fun createFromParcel(parcel: Parcel): Article {
+            return Article(parcel)
+        }
+
+        override fun newArray(size: Int): Array<Article?> {
+            return arrayOfNulls(size)
+        }
+    }
+}
---
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/ArticleAdapter.kt
@@ -0,0 +1,47 @@
+package com.ioh_c22_h2_4.hy_ponics
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.ioh_c22_h2_4.hy_ponics.databinding.ItemArticleBinding
+
+class ArticleAdapter(private val articleList: ArrayList<Article>) :
+    RecyclerView.Adapter<ArticleAdapter.MyViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val binding = ItemArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return MyViewHolder(binding)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val data = articleList[position]
+        holder.bind(data)
+    }
+
+    class MyViewHolder(private val binding: ItemArticleBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(data: Article) {
+            Glide.with(binding.root.context)
+                .load(data.img)
+                .into(binding.articleImage)
+            binding.articleTitle.text = data.title
+
+//            binding.root.setOnClickListener {
+//                val article = Article(
+//                    data.title,
+//                    data.img,
+//                    data.content
+//                )
+
+//                val intent = Intent(binding.root.context, DetailArticleFragment::class.java)
+//                intent.putExtra(DetailArticleFragment.DETAIL_ARTICLE, article)
+//                binding.root.context.startActivity(intent)
+//            }
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return articleList.size
+    }
+}
