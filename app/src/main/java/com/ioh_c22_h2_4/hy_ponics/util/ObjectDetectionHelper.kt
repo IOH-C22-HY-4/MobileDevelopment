afa3ab4f09716c9bcd723b3d7b12916adffea946
onirutlA <alturino001@gmail.com>
2022-06-13 16:12:18 +0700

    refactor: remove unneeded comment

--- app/src/main/java/com/ioh_c22_h2_4/hy_ponics/util/ObjectDetectionHelper.kt
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/util/ObjectDetectionHelper.kt
@@ -7,19 +7,12 @@ import java.nio.ByteOrder
 
 class ObjectDetectionHelper(private val tflite: Interpreter, private val labels: List<String>) {
 
-    /** Abstraction object that wraps a prediction output in an easy to parse way */
-
     private val scores = arrayOf(FloatArray(OBJECT_COUNT))
 
     val predictions
         get() = (0 until OBJECT_COUNT).map {
             PredictionResult(
-                // SSD Mobilenet V1 Model assumes class 0 is background class
-                // in label file and class labels start from 1 to number_of_classes + 1,
-                // while outputClasses correspond to class index from 0 to number_of_classes
                 label = labels[it],
-
-                // Score is a single value of [0, 1]
                 score = scores[0][it]
             )
         }
