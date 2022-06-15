158a0d96cabc14e040fa900694006a2325ddd6f8
Vincent-Sirait <66838524+Vincent-Sirait@users.noreply.github.com>
2022-06-15 22:49:30 +0700

    menghapus crashlytics dan memperbaiki splash screen

--- app/src/main/java/com/ioh_c22_h2_4/hy_ponics/SplashScreenActivity.kt
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/SplashScreenActivity.kt
@@ -5,6 +5,7 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.google.firebase.auth.FirebaseAuth
 import com.ioh_c22_h2_4.hy_ponics.databinding.ActivitySplashScreenBinding
 import kotlinx.coroutines.delay
 
@@ -13,12 +14,25 @@ class SplashScreenActivity : AppCompatActivity() {
 
     private val binding by lazy { ActivitySplashScreenBinding.inflate(layoutInflater) }
 
+    private lateinit var auth: FirebaseAuth;
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
 
+        auth = FirebaseAuth.getInstance()
+
         lifecycleScope.launchWhenCreated {
             delay(2000)
+            checkUser()
+        }
+    }
+
+    private fun checkUser() {
+        val firebaseUser = auth.currentUser
+        if (firebaseUser == null) {
+            startActivity(Intent(this, LoginActivity::class.java))
+        } else {
             Intent(this@SplashScreenActivity, MainActivity::class.java).also {
                 startActivity(it)
                 finish()
