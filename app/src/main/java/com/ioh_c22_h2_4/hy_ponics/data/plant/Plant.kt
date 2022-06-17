67dbbd06abb0b5d4229ee7cdeb605fffeb8806c1
onirutlA <alturino001@gmail.com>
2022-06-17 13:29:25 +0700

    refactor: add default value to Plant.kt

--- app/src/main/java/com/ioh_c22_h2_4/hy_ponics/data/plant/Plant.kt
+++ app/src/main/java/com/ioh_c22_h2_4/hy_ponics/data/plant/Plant.kt
@@ -1,13 +1,13 @@
 package com.ioh_c22_h2_4.hy_ponics.data.plant
 
 data class Plant(
-    val ECmax: Double,
-    val ECmin: Double,
-    val TDSmax: Int,
-    val TDSmin: Int,
-    val ecImage: String,
-    val pHmax: Double,
-    val pHMin: Double,
-    val phImage: String,
-    val tdsImage: String,
+    val ECmax: Double = 0.0,
+    val ECmin: Double = 0.0,
+    val TDSmax: Int = 0,
+    val TDSmin: Int = 0,
+    val ecImage: String = "",
+    val pHmax: Double = 0.0,
+    val pHmin: Double = 0.0,
+    val phImage: String = "",
+    val tdsImage: String = "",
 )
